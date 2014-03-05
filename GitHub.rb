50372c837da74c13f74b20ad7948c78bca7471d8
Jayant Biswas <jayant.biswas@neurobat.net>
2014-03-05 10:19:35 +0100

    This fixes #1

--- GitHub.rb
+++ GitHub.rb
@@ -28,8 +28,8 @@ class GitHub
     req = Net::HTTP::Post.new(url.to_s)
     req.basic_auth("token", @auth_token)
     req["content-type"] = "application/json"
-    finalComment = ":moyai: Gictatr[almost reliable] says: #{comment}\n\n"
-    finalComment += "If you think there is a bug in Gictatr's judgement please "
+    finalComment = ":moyai: Gictator[almost reliable] says: #{comment}\n\n"
+    finalComment += "If you think there is a bug in Gictator's judgement please "
     finalComment += "create an issue here "
     finalComment += "https://github.com/jbiswas/git-tools/issues/new"
     req.body={:body => finalComment}.to_json
