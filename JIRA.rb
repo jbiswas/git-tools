bb758cb270b3742a643b66b5d92d701cd06da817
Jayant Biswas <jayant.biswas@neurobat.net>
2014-12-01 11:48:20 +0100

    Get closed issues in each sprint by assignee

--- JIRA.rb
+++ JIRA.rb
@@ -29,6 +29,29 @@ class JIRA
 
   def get_issues_in_sprint()
     url = URI("https://bugs.neurobat.net/rest/api/2/search?jql=Sprint+in+OpenSprints()")
+    req = Net::HTTP::Get.new(url.to_s)
+    req.basic_auth(@user, @pass)
+    req["content-type"] = "application/json"
+
+    res = Net::HTTP.start(url.host, url.port, :use_ssl => true) do |http|
+      response = http.request(req)
+    end
+
+    result_list = []
+
+    if res.code == "200"
+      result = JSON.parse(res.body)
+      result["issues"].each do |issue|
+        if issue["fields"]["issuetype"]["subtask"] == false
+          result_list << issue["key"]
+        end
+      end
+    end
+    return result_list
+  end
+
+  def get_closed_issues_in_sprint(assignee)
+    url = URI("https://bugs.neurobat.net/rest/api/2/search?jql=Sprint+in+OpenSprints()+AND+status+in+(Closed,Resolved,%22Under+test%22)+AND+assignee=#{assignee}")
 
     req = Net::HTTP::Get.new(url.to_s)
     req.basic_auth(@user, @pass)
