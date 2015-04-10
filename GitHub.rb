c5480416a567e8f8dc473d049071cc60b23884f5
Jayant Biswas <jayant.biswas@neurobat.net>
2015-04-10 16:31:13 +0200

    Remove "almost reliable"

--- GitHub.rb
+++ GitHub.rb
@@ -47,7 +47,7 @@ class GitHub
     req = Net::HTTP::Post.new(url.to_s)
     req.basic_auth("token", @auth_token)
     req["content-type"] = "application/json"
-    finalComment = ":moyai: Gictator[almost reliable] says: #{comment}\n\n"
+    finalComment = ":moyai: Gictator says: #{comment}\n\n"
     finalComment += "If you think there is a bug in Gictator's judgement please "
     finalComment += "create an issue here "
     finalComment += "https://github.com/jbiswas/git-tools/issues/new"
