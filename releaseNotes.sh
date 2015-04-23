9cf10a77192d766478c1a30a0a99a0b1c4854714
Xavier Mocellin <xavier.mocellin@neurobat.net>
2015-04-23 16:34:57 +0200

    idothankyounowforreleaseNotes.sh

---
+++ releaseNotes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo " " > foo.tmp
+git lol | grep $1 >> foo.tmp
+awk 'FS="]" {print $1}' foo.tmp | awk 'FS="[" {print "key="$2}' | sort -uV | xargs -l ~/neurobat/git-tools/get_issue.rb | sort -k3
+rm foo.tmp
