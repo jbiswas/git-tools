376dbb195a76826dcee5ddc8f0240ae1451ebfd6
Xavier Mocellin <xavier.mocellin@neurobat.net>
2015-11-06 09:48:59 +0100

    Fix issues selection and clean jira authentication

--- get_issue.rb
+++ get_issue.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
-
-$LOAD_PATH << '.'
 require_relative 'JIRA'
 
-#j = JIRA.new('username', 'password')
+j = JIRA.new()
 a = j.get_issue_details("key="+ARGV[0])
 if a.empty?
 else
-  printf("%-10s %-11s %-13s %-11s %-11s %s\n", ARGV[0].sub("key=",""), a[0], a[1], a[2][0,10], a[3][0,10], a[4])
+  printf("%s \t %s \t %s \t  %s \t %s \t %s \t %s\n", ARGV[0].sub("key=",""), a[0], a[1], a[2], a[3][0,10], a[4][0,10], a[5])
 end
--- releaseNotes.sh
+++ releaseNotes.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git lol $1 | grep "\[" | sed 's/.*\[//' | awk 'FS="]" {print $1}' | sed 's/.$//' | sort -uV | xargs -l ~/neurobat/git-tools/get_issue.rb | sort -k5
+git lol $1 | grep "\[" | sed 's/.*\[//' | awk 'FS="]" {print $1}' | sed -e 's/\]//' | sort -uV | xargs -l ~/neurobat/git-tools/get_issue.rb | sort -k6
