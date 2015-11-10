78e7a0500c79416846370692cf1f99964110a307
Xavier Mocellin <xavier.mocellin@neurobat.net>
2015-11-10 12:06:46 +0100

    No more path dependancies, output increased

--- releaseNotes.sh
+++ releaseNotes.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-git lol $1 | grep "\[" | sed 's/.*\[//' | awk 'FS="]" {print $1}' | sed -e 's/\]//' | sort -uV | xargs -l ~/neurobat/git-tools/get_issue.rb | sort -k6
+t=$1
+tag=${t#*..*}
+git log --grep "^\[" --no-merges --pretty=format:"%s ] %ad" --date=short $1 | sed 's/.*\[//' | awk 'BEGIN {FS="]"; RS="\n"} NF==3{print $1 $3}' | sort -u | awk '{a[$1] = $0} END{for (i in a) print a[i] " '$tag'"}' | xargs -l $(dirname $0)/get_issue.rb | sort -k6
+
